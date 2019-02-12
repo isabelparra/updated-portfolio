9c67c599186b64934345d556ba3e4b132d2df1d9
Isabel Parra <isabelparra@Isabels-MacBook-Pro.local>
2019-02-11 23:20:22 -0500

    typerwriter header

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,275 @@
 
+  // $(document).ready(function() {
+  //   var e=new Typewriter(
+  //     $("#heading")[0],
+  //     {loop:!0,
+  //     blinkSpeed:50}
+  //   );
+  //   e.typeString("Type").pauseFor(850) });
+
+
+    //    document.addEventListener("DOMContentLoaded", function(){
+    //     Typed.new(".element", {
+    //         strings: ["First sentence.", "Second sentence."],
+    //         typeSpeed: 0
+    //     });
+    // });
+
+    $(function() {
+        // $("#typed").typed({
+        var typed = new Typed('#typed', {
+          stringsElement: "#typed-strings",
+              // $("#typed").typed({
+          // strings: 
+          // ["Hi I'm Isabel^500"],
+          // "I'm a Graphic_Designer"],
+      
+       
+             
+      
+              // strings: jquery plugin types sentences and deletes them
+            //   strings: ["Hi I'm Isabel","I'm a Graphic Designer","an Illustrator"],
+         typeSpeed: 30, 
+         startDelay: 0,
+          backSpeed: 0,
+          backDelay: 800,
+          loop: false,
+              contentType: 'html',
+              loopCount: true,
+              // cursorChar: "",
+              callback: function() {},
+               // starting callback function before each string
+          // preStringTyped: function() {},
+          
+          // //callback for every typed string
+          // onStringTyped: function() {},
+          
+          // // callback for reset
+         resetCallback: function() {
+          //  showThis();
+         }
+        });
+      
+      
+            // })
+       
+        });
+            
+      //       $(function() {
+      //         $(".testing-typed").typed({
+      //           stringsElement: $('.typed-effect'),
+      //             strings: ["Hi I'm Isabel^200", "I'm a Graphic_Designer", "an Illustrator", "UX/UI_Designer", "and Yoga_Teacher"],
+      //             typeSpeed: 30,
+      //             startDelay: 0,
+      //             smartBackspace: true;
+      //           backSpeed: 0,
+      //         backDelay: 800,
+      //         // showCursor: false,
+      //         // contentType: 'html',
+      //         loop: false,
+      //         // contentType: 'html',
+      //         // loopCount: false,
+      //             callback: function() {
+      
+      //               // showThis();
+      //             },
+                
+      //         });
+      // // $(".reset").click(function() {
+      // //         $("h2 .first").typed('reset');
+      // //       });
+      // };
+         
+          //  function showThis(){
+      
+          //     $("#typed").typed({
+          //       // stringsElement: $('.second'),
+          //           strings: ["a"],
+          //           backDelay: 1500,    
+          //           typeSpeed: 100,
+          //           backSpeed: 100,
+          //           loop: true
+                    
+                    
+          //       });
+          //     };
+      
+                // function and(){
+                //   $("span.second").typed({
+                //     strings: ["and yoga teacher"],
+                //     typeSpeed: 30,
+                //     backSpeed: 30,
+                //     loop: false,
+                //   })
+                // }
+      
+        //     }
+        //  });
+      
+      
+      // typewriter.typeString('Hello')
+      // .pauseFor(2500)
+      // .deleteAll()
+      // .typeString("I'm Isa");
+      // .start();
+      
+      
+        
+          // function newTyped() {
+          //   /* typed obj */
+          // }
+      
+      
+      
+      
+      
+        
+
+
+
+
+
+
+
+
+
+// !function(t){
+//     "use strict";
+//     var s=function(s,o){
+//         this.el=t(s),
+//         this.options=t.extend({},
+//             t.fn.typed.defaults,o),
+//             this.isInput=this.el.is("input"),
+//             this.attr=this.options.attr,
+//             this.showCursor=this.isInput?!1:this.options.showCursor,
+//             this.elContent=this.attr?this.el.attr(this.attr):this.el.text(),
+//             this.contentType=this.options.contentType,
+//             this.typeSpeed=this.options.typeSpeed,
+//             this.startDelay=this.options.startDelay,
+//             this.backSpeed=this.options.backSpeed,
+//             this.backDelay=this.options.backDelay,
+//             this.strings=this.options.strings,
+//             this.strPos=0,this.arrayPos=0,
+//             this.stopNum=0,this.loop=this.options.loop,
+//             this.loopCount=this.options.loopCount,
+//             this.curLoop=0,this.stop=!1,
+//             this.cursorChar=this.options.cursorChar,this.build()};s.prototype={constructor:s,init:function(){var t=this;t.timeout=setTimeout(function(){t.typewrite(t.strings[t.arrayPos],t.strPos)},t.startDelay)},build:function(){this.showCursor===!0&&(this.cursor=t('<span class="typed-cursor">'+this.cursorChar+"</span>"),this.el.after(this.cursor)),this.init()},typewrite:function(t,s){if(this.stop!==!0){var o=Math.round(70*Math.random())+this.typeSpeed,e=this;e.timeout=setTimeout(function(){var o=0,i=t.substr(s);if("^"===i.charAt(0)){var r=1;/^\^\d+/.test(i)&&(i=/\d+/.exec(i)[0],r+=i.length,o=parseInt(i)),t=t.substring(0,s)+t.substring(s+r)}if("html"===e.contentType){var n=t.substr(s).charAt(0);if("<"===n||"&"===n){var a="",h="";for(h="<"===n?">":";";t.substr(s).charAt(0)!==h;)a+=t.substr(s).charAt(0),s++;s++,a+=h}}e.timeout=setTimeout(function(){if(s===t.length){if(e.options.onStringTyped(e.arrayPos),e.arrayPos===e.strings.length-1&&(e.options.callback(),e.curLoop++,e.loop===!1||e.curLoop===e.loopCount))return;e.timeout=setTimeout(function(){e.backspace(t,s)},e.backDelay)}else{0===s&&e.options.preStringTyped(e.arrayPos);var o=t.substr(0,s+1);e.attr?e.el.attr(e.attr,o):e.isInput?e.el.val(o):"html"===e.contentType?e.el.html(o):e.el.text(o),s++,e.typewrite(t,s)}},o)},o)}},backspace:function(t,s){if(this.stop!==!0){var o=Math.round(70*Math.random())+this.backSpeed,e=this;e.timeout=setTimeout(function(){if("html"===e.contentType&&">"===t.substr(s).charAt(0)){for(var o="";"<"!==t.substr(s).charAt(0);)o-=t.substr(s).charAt(0),s--;s--,o+="<"}var i=t.substr(0,s);e.attr?e.el.attr(e.attr,i):e.isInput?e.el.val(i):"html"===e.contentType?e.el.html(i):e.el.text(i),s>e.stopNum?(s--,e.backspace(t,s)):s<=e.stopNum&&(e.arrayPos++,e.arrayPos===e.strings.length?(e.arrayPos=0,e.init()):e.typewrite(e.strings[e.arrayPos],s))},o)}},reset:function(){var t=this;clearInterval(t.timeout);var s=this.el.attr("id");this.el.after('<span id="'+s+'"/>'),this.el.remove(),"undefined"!=typeof this.cursor&&this.cursor.remove(),t.options.resetCallback()}},t.fn.typed=function(o){return this.each(function(){var e=t(this),i=e.data("typed"),r="object"==typeof o&&o;i||e.data("typed",i=new s(this,r)),"string"==typeof o&&i[o]()})},t.fn.typed.defaults={strings:["These are the default values...","You know what you should do?","Use your own!","Have a great day!"],typeSpeed:0,startDelay:0,backSpeed:0,backDelay:500,loop:!1,loopCount:!1,showCursor:!0,cursorChar:"|",attr:null,contentType:"html",callback:function(){},preStringTyped:function(){},onStringTyped:function(){},
+
+// resetCallback:function(){}}}(window.jQuery);
+
+
+
+
+// // My code
+// $('.resource').hide();
+//  $(function(){
+//   $(".typed-effect").typed({
+//     strings: ["Hi I'm Isabel^200"], // ^{number} denotes time in ms to wait
+
+//     // typing speed
+//     typeSpeed: 70,
+    
+//     // time before typing starts
+//     startDelay: 0,
+    
+//     // backspacing speed
+//     backSpeed: 0,
+    
+//     // time before backspacing
+//     backDelay: 50,
+    
+//     // loop
+//     loop: false,
+    
+//     // false = infinite
+//     loopCount: false,
+    
+//     // show cursor
+//     showCursor: true,
+    
+//     // character for cursor
+//     cursorChar: "",
+    
+//     // attribute to type (null == text)
+//     attr: null,
+    
+//     // either html or text
+//     contentType: 'html',
+    
+//     // call when done callback function
+//     callback: function() {
+
+//        $('.resource').delay(500).fadeIn('slow');
+//       setTimeout (function () {
+//         $('.typed-effect + .typed-cursor').css({'opacity': '0', 'animation': 'none'});
+//       }, 1300);
+
+//     },
+    
+//     // starting callback function before each string
+//     preStringTyped: function() {},
+    
+//     //callback for every typed string
+//     onStringTyped: function() {},
+    
+//     // callback for reset
+//     resetCallback: function() {}
+//   });
+//   setTimeout ( function() {
+//     $(".testing-typed").typed({
+//     strings: ["I am a Graphic_Designer"], // ^{number} denotes time in ms to wait
+
+//     // typing speed
+//     typeSpeed: 120,
+    
+//     // time before typing starts
+//     startDelay: 0,
+    
+//     // backspacing speed
+//     backSpeed: 0,
+    
+//     // time before backspacing
+//     backDelay: 800,
+    
+//     // loop
+//     loop: true,
+    
+//     // false = infinite
+//     loopCount: false,
+    
+//     // show cursor
+//     showCursor: true,
+    
+//     // character for cursor
+//     cursorChar: "",
+    
+//     // attribute to type (null == text)
+//     attr: null,
+    
+//     // either html or text
+//     contentType: 'html',
+    
+//     // call when done callback function
+//     callback: function() {},
+    
+//     // starting callback function before each string
+//     preStringTyped: function() {},
+    
+//     //callback for every typed string
+//     onStringTyped: function() {},
+    
+//     // callback for reset
+//     resetCallback: function() {}
+//   });
+//   }, 6500);
+// });
+
+
+
+
+
+
+
   $('.sidenav').sidenav();
   $('.parallax').parallax();
 //   $('.category').hide();
@@ -22,8 +293,66 @@
             //     $('.category').css("visibility", "hidden");
             //     });
             
-      
 
+
+
+
+
+    //   var TxtType = function(el, toRotate, period) {
+    //     this.toRotate = toRotate;
+    //     this.el = el;
+    //     this.loopNum = 0;
+    //     this.period = parseInt(period, 10) || 2000;
+    //     this.txt = '';
+    //     this.tick();
+    //     this.isDeleting = false;
+    // };
+    // TxtType.prototype.tick = function() {
+    //     var i = this.loopNum % this.toRotate.length;
+    //     var fullTxt = this.toRotate[i];
+
+    //     if (this.isDeleting) {
+    //     this.txt = fullTxt.substring(0, this.txt.length - 1);
+    //     } else {
+    //     this.txt = fullTxt.substring(0, this.txt.length + 1);
+    //     }
+
+    //     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    //     var that = this;
+    //     var delta = 200 - Math.random() * 100;
+
+    //     if (this.isDeleting) { delta /= 2; }
+
+    //     if (!this.isDeleting && this.txt === fullTxt) {
+    //     delta = this.period;
+    //     this.isDeleting = true;
+    //     } else if (this.isDeleting && this.txt === '') {
+    //     this.isDeleting = false;
+    //     this.loopNum++;
+    //     delta = 500;
+    //     }
+
+    //     setTimeout(function() {
+    //     that.tick();
+    //     }, delta);
+    // };
+
+    // window.onload = function() {
+    //     var elements = document.getElementsByClassName('typewrite');
+    //     for (var i=0; i<elements.length; i++) {
+    //         var toRotate = elements[i].getAttribute('data-type');
+    //         var period = elements[i].getAttribute('data-period');
+    //         if (toRotate) {
+    //           new TxtType(elements[i], JSON.parse(toRotate), period);
+    //         }
+    //     }
+    //     // INJECT CSS
+    //     var css = document.createElement("style");
+    //     css.type = "text/css";
+    //     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    //     document.body.appendChild(css);
+    // };
 
 
 
@@ -37,9 +366,9 @@
    
     });
 
-    // $grid.imagesLoaded().progress(function() {
-    //   $grid.masonry('layout');
-    // });
+//     // $grid.imagesLoaded().progress(function() {
+//     //   $grid.masonry('layout');
+//     // });
 
 
 
@@ -165,3 +494,35 @@
 //   percentPosition: true
 //   // isFitWidth: true
 // });
+
+// $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+//     event.preventDefault();
+//     $(this).ekkoLightbox({
+//         alwaysShowClose: true,
+//         onShown: function() {
+//             console.log('Portfolio Img');
+//         }
+//     });
+//     // $(".modal-content").modal
+// });
+
+// var type = document.getElementById('type');
+
+// var typewriter = new Typewriter(app, {
+//     loop: true
+// });
+
+// typewriter.typeString('Hello')
+// .pauseFor(2500)
+// .deleteAll()
+// .typeString('Graphic Designer')
+// .pauseFor(2500)
+// .deleteChars(7)
+// .typeString('yoga')
+// .pauseFor(2500)
+// .start();
+
+
+
+
+
