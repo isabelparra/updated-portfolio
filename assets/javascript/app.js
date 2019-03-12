7870049caf7efedf2c3d12d8610e7da198f370d5
Isabel Parra <isabelparra@Isabels-MacBook-Pro.local>
2019-03-12 16:23:22 -0400

    mobile grid
    Former-commit-id: 634cced4097c4858757722cb3eab52b535d831b3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,23 @@
+
+
+// $('.grid').masonry({ 
+//   itemSelector: '.grid-item',
+//   columnWidth: '.grid-sizer',
+//    percentPosition: true
+//   });
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    // columnWidth: '.grid-sizer',
+     isFitWidth: true,
+     columnWidth: 1
+    // options...
+    });
+
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+  });
+  
+
 // setTimeout(
 //   function() { 
 //   masonry() }
@@ -10,56 +30,23 @@
 //       $projects.masonry();
 //   });
 // });
-
-var $grid = $('.grid').imagesLoaded( function() {
-  $grid.masonry({  
-      itemSelector: '.grid-item',
-      percentPosition: true,
-      columnWidth: '.grid-sizer',
-      gutter: '.gutter-sizer',
-
- 
-  });
-
+// var $grid = $('.grid').imagesLoaded( function() {
+//   $grid.masonry({  
+//       itemSelector: '.grid-item',
+//       percentPosition: true,
+//       columnWidth: '.grid-sizer',
+//       gutter: '.gutter-sizer',
+//   });
+// });
   // $grid.imagesLoaded().progress(function() {
   //   $grid.masonry('layout');
   // });
-});
-
-$('.grid').masonry({ 
-itemSelector: '.grid-item',
-columnWidth: '.grid-sizer',
- percentPosition: true
-// gutter: '.gutter-sizer',
-
-});
-
-var $grid = $('.grid').masonry({
-// options...
-});
-
-$grid.imagesLoaded().progress( function() {
-$grid.masonry('layout');
-});
-
-
-
-
-
 //   $('.grid-item').click(function() {
 //     $('.lightbox').fadeIn(300);
 //       $('.lightbox').append("<img src='" + $(this).attr('src') + "'alt='" + $(this).attr("alt") + "' />");
 //       $(".filter").css("background-image", "url(" + $(this).attr("src") + ")");
 
 // });
-
-
-
-
-
-
-
-
 // $('.grid-item').click(function(event) {
 //   if(!$(this).hasClass('item-opened')) {
 //     var elWidth = $(this).outerWidth()/2;
@@ -80,9 +67,7 @@ $grid.masonry('layout');
 //               // marginLeft:	'-20%'
 //               // position: 'fixed'
 //             }).addClass('item-opened');
-
 //           $('.grid-alpha').fadeIn();
-
 //           // Scroll to the top
 //           $('html, body').animate({
 //             scrollTop: $('.grid').offset().top
@@ -92,8 +77,6 @@ $grid.masonry('layout');
 //           $('.grid').css('overflow', 'hidden');
 //         }
 //     });
-
-
 // Close item Modal
   // $(document).on('click', function(e) {
   //   if ($('.item-opened').length > 0) {
@@ -117,33 +100,23 @@ $grid.masonry('layout');
   //     }
   //   }
   // });
-
-
-
-
-
 //   $(document).on('click', '.click-to-expand', function() {
 //     var imageSrc = $(this).parents('.image-grid').find('img').attr('src');
 //     $('.js-modal-image').attr('src', imageSrc);
 //   });
-
 // (function() {
 //   $(document).on("click", ".click-to-expand", function() {
 //     var imageSrc = $(this).parents(".image-grid").find("img").attr("src");
 //     $(".js-modal-image").attr("src", imageSrc);
 //   });
 // })();
-
 // $('.grid').masonry({
 //   // options
 //   // itemSelector: '.grid-item',
 //   // columnWidth: 33.3%;
-
-
 // });
 
 // init Masonry after all images have loaded//
-
 
 // $('.grid').masonry({
 //   itemSelector: '.grid-item',
@@ -152,12 +125,9 @@ $grid.masonry('layout');
 //   percentPosition: true
 //   // isFitWidth: true
 // });
-
-
 $(function() {
   //  $("#typed").typed({
-
-var typed = new Typed("#typed", {
+  var typed = new Typed("#typed", {
   // stringsElement: "#typed-strings",
   strings: ["HELLO I'M ISABEL"],
     typeSpeed: 0, 
